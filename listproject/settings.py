f4ebd0729612871c68b6bd9f35708fba424646aa
Henry Koekemoer <henry@sensornetworks.co.za>
2021-12-13 13:54:48 +0200

    Config update

--- listproject/settings.py
+++ listproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4dog#kwfxl4^@)fw(cbzc^xe=e9c%hd4*qqhig-ek_47trgr-b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://trello-pizza-project.herokuapp.com/']
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
